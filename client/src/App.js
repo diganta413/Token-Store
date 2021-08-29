93a07bf9d0f7f35c3d2d97b86d4a9921de0203c8
DebjitPramanick <dpramanick9990@gmail.com>
2021-08-29 17:49:12 +0530

    Dummy change

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 class App extends Component {
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
+  
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
